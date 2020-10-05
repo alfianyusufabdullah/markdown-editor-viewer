cfdc101ace9c25137c5b4e5a9cdf5f3f617d3342
alfianyusufabdullah <alfianyabdullah@gmail.com>
2020-10-05 21:27:48 +0700

    Initial project :tada:

---
+++ app/src/main/java/alfianyusufabdullah/exp/markdown/MainActivity.kt
@@ -0,0 +1,11 @@
+package alfianyusufabdullah.exp.markdown
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

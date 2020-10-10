335fdf268f4c8a636df6d8039dae45968746ac1b
Alfian Yusfu Abdullah <alfianyabdullah@gmail.com>
2020-10-10 21:59:39 +0700

    fixed failed running mapping markdown

--- app/src/main/java/alfianyusufabdullah/exp/markdown/MarkdownViewModel.kt
+++ app/src/main/java/alfianyusufabdullah/exp/markdown/MarkdownViewModel.kt
@@ -6,10 +6,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.FlowPreview
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.debounce
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import kotlin.time.ExperimentalTime
 import kotlin.time.milliseconds
@@ -18,17 +15,33 @@ import kotlin.time.milliseconds
 @FlowPreview
 class MarkdownViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>()
-    val text: LiveData<String> = _text
+    private val _markdown = MutableLiveData<String>()
+    val markdown: LiveData<String> = _markdown
+
+    private val _html = MutableLiveData<String>()
+    val html: LiveData<String> = _html
 
     fun onTextChanged(text: String) {
         viewModelScope.launch {
+
+            flow {
+                emit(text)
+            }
+                .debounce(1000.milliseconds)
+                .map { MarkdownHelper.codeBlockToHtml(it) }
+                .map { MarkdownHelper.singleLineFormatter(it, MarkdownHelper.TYPE_BOLD) }
+                .map { MarkdownHelper.singleLineFormatter(it, MarkdownHelper.TYPE_ITALIC) }
+                .map { MarkdownHelper.singleLineFormatter(it, MarkdownHelper.TYPE_INLINE_CODE) }
+                .flowOn(Dispatchers.Main)
+                .collect { _html.postValue(it) }
+
             flow { emit(text) }.apply {
                 debounce(1000.milliseconds)
                 collect {
-                    _text.postValue(it)
+                    _markdown.postValue(it)
                 }
             }.flowOn(Dispatchers.Main)
+
         }
     }
 }
